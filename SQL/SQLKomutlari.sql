df610da8da09d21fe4d423f4a0ec1ec906d32d56
Fırat Alçın <firatalcin04@gmail.com>
2025-02-17 22:21:16 +0300

    Select and Insert

---
+++ SQL/SQLKomutlari.sql
@@ -0,0 +1,9 @@
+--Select 
+
+select ID, CUSTOMERNAME, CITY, BIRTHDATE, DISTRICT, GENDER, NATION, AGE from CUSTOMERS
+
+--Insert
+
+insert into CUSTOMERS (CUSTOMERNAME, CITY, BIRTHDATE, DISTRICT, GENDER, NATION, AGE) values
+('Mehmet Çınar', 'Tokat', '19550101', 'Erbaa', 'E', 'TR', 70)
+
