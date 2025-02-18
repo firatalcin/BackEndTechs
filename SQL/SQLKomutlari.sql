8c9c15d0d15850e8e3ccc5a67f0d4377fb3bb2ff
Fırat Alçın <firatalcin04@gmail.com>
2025-02-18 23:58:07 +0300

    Update and Delete

--- SQL/SQLKomutlari.sql
+++ SQL/SQLKomutlari.sql
@@ -7,3 +7,13 @@ select ID, CUSTOMERNAME, CITY, BIRTHDATE, DISTRICT, GENDER, NATION, AGE from CUS
 insert into CUSTOMERS (CUSTOMERNAME, CITY, BIRTHDATE, DISTRICT, GENDER, NATION, AGE) values
 ('Mehmet Çınar', 'Tokat', '19550101', 'Erbaa', 'E', 'TR', 70)
 
+--Update
+
+update CUSTOMERS set NATION = 'TR'
+
+--Delete
+
+delete from CUSTOMERS
+
+
+
