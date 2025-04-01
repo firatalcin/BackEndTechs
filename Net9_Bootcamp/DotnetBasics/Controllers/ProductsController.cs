3157a5a4df0dcd1ffbc370d7b0ee0424a0af841d
Fırat Alçın <firatalcin04@gmail.com>
2025-04-01 21:52:32 +0300

    Veri Türleri

--- Net9_Bootcamp/DotnetBasics/Controllers/ProductsController.cs
+++ Net9_Bootcamp/DotnetBasics/Controllers/ProductsController.cs
@@ -17,6 +17,18 @@ namespace DotnetBasics.Controllers
         
         public ActionResult Details()
         {
+            string urunBaslik = "Samsung S24 Ultra";
+            string urunAciklama = "Samsung S24 Ultra 512 GB 12 GB RAM";
+            double urunFiyat = 69999.99;
+            string urunResim = "samsung.jpg";
+            bool urunSatistami = false;
+            
+            ViewData["urunResim"] = urunResim;
+            ViewData["urunBaslik"] = urunBaslik;
+            ViewData["urunAciklama"] = urunAciklama;
+            ViewData["urunFiyat"] = urunFiyat;
+            ViewData["urunSatistami"] = urunSatistami;
+            
             return View();
         }
 
