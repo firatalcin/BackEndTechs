70166164a1c135030bab242de957f193168a1260
Fırat Alçın <firatalcin04@gmail.com>
2025-04-01 21:42:30 +0300

    Değişken Tanımlama

--- Net9_Bootcamp/DotnetBasics/Controllers/HomeController.cs
+++ Net9_Bootcamp/DotnetBasics/Controllers/HomeController.cs
@@ -6,6 +6,17 @@ namespace DotnetBasics.Controllers
     {
         public ActionResult Index()
         {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            //int urunFiyat = 5000;
+            
+            sayi1 = 30;
+            sayi2 = 40;
+            
+            int  sonuc = sayi1 + sayi2;
+            
+            ViewData["sonuc"] = sonuc;
+            
             return View();
         }
 
