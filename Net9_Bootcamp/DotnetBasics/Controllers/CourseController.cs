ae30ae4b2c4f6b6ecffcb9d9b39c79359bfede49
Fırat Alçın <firatalcin04@gmail.com>
2025-04-02 21:57:34 +0300

    Diziler

--- Net9_Bootcamp/DotnetBasics/Controllers/CourseController.cs
+++ Net9_Bootcamp/DotnetBasics/Controllers/CourseController.cs
@@ -11,6 +11,32 @@ namespace DotnetBasics.Controllers
 
         public ActionResult List()
         {
+            // string kursAdi1 = "Javascript Kursu";
+            // string kursAdi2 = "React Kursu";
+
+            // string kursAciklama1 = "Javascript Kurs Açıklaması";
+            // string kursAciklama2 = "React Kurs Açıklaması";
+
+            string[] kursAdlari = ["Javascript Kursu", "React Kursu"];
+            string[] kursAciklamalari = ["Javascript Kurs Açıklaması", "React Kurs Açıklaması"];
+            string[] kursResimleri = ["1.jpg", "2.jpg"];
+
+            ViewData["kursAdi1"] = kursAdlari[0]; // Javascript Kursu
+            ViewData["kursAdi2"] = kursAdlari[1]; // React Kursu
+
+            ViewData["kursAciklama1"] = kursAciklamalari[0]; // Javascript Kurs Açıklaması
+            ViewData["kursAciklama2"] = kursAciklamalari[1]; // React Kurs Açıklaması
+
+            ViewData["kursResim1"] = kursResimleri[0]; // 1.jpg
+            ViewData["kursResim2"] = kursResimleri[1]; // 2.jpg
+
+            // ViewData["kursAdlari"] = kursAdlari;
+            // ViewData["kursAciklamalari"] = kursAciklamalari;
+            // ViewData["kursResimleri"] = kursResimleri;
+            // @(((string[])ViewData["kursAdlari"])[0])
+
+            return View();
+            
             return View();
         }
         
