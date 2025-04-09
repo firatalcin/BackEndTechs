5806378fecd4dfbd150a6e5bb16b56402f530f7c
Fırat Alçın <firatalcin04@gmail.com>
2025-04-09 22:56:05 +0300

    Entity Sınıfı

---
+++ Net9_Bootcamp/DotnetStore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Net9_Bootcamp/DotnetStore/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetStore.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+}
