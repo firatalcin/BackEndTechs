84a7468284da9a266421599357878ca57473bbd5
Fırat Alçın <firatalcin04@gmail.com>
2025-04-09 23:13:02 +0300

    Connection Strings

--- Net9_Bootcamp/DotnetStore/Program.cs
+++ Net9_Bootcamp/DotnetStore/Program.cs
@@ -1,7 +1,17 @@
+using DotnetStore.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(optionsAction: options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlite(connectionString);
+
+});
 
 var app = builder.Build();
 
