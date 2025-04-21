164df97f0acd29c8ead90da332574bc12b359d21
Fırat Alçın <firatalcin04@gmail.com>
2025-04-21 22:40:23 +0300

    Navbar ViewComponent

---
+++ Net9_Bootcamp/DotnetStore/ViewComponents/Navbar.cs
@@ -0,0 +1,19 @@
+﻿using DotnetStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetStore.ViewComponents;
+
+public class Navbar : ViewComponent
+{
+    private readonly AppDbContext _context;
+
+    public Navbar(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View(_context.Categories.ToList());
+    }
+}
